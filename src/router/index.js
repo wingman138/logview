ea1bbe5167ba29881654f82aec241860954bf0eb
chg <13817411976@139.com>
2024-01-09 17:28:11 +0800

    修改了关于排队页面的路由

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,26 @@ const router = createRouter({
           component: () => import('@/views/logs/LogView.vue')
         },
         {
+          path: '/queue/category',
+          component: () => import('@/views/queue/CategorySetting.vue')
+        },
+        {
+          path: '/queue/button',
+          component: () => import('@/views/queue/ButtonSetting.vue')
+        },
+        {
+          path: '/queue/ticket',
+          component: () => import('@/views/queue/TicketSetting.vue')
+        },
+        {
+          path: '/queue/priority',
+          component: () => import('@/views/queue/PrioritySetting.vue')
+        },
+        {
+          path: '/queue/page',
+          component: () => import('@/views/queue/PageSetting.vue')
+        },
+        {
           path: '/other/building',
           component: () => import('@/views/other/BuildingPage.vue')
         }
