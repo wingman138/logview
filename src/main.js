26b6bf75c8fbd10c85b8c11bc9530942dd8e9425
chg <13817411976@139.com>
2024-02-05 15:37:40 +0800

    增加了号票、优先级、界面管理

--- src/main.js
+++ src/main.js
@@ -2,16 +2,21 @@ import { createApp } from 'vue'
 // import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-
+// import 'default-passive-events'
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.scss'
-
+// 统一注册所有 element-ui 图标
+import { loadIcon } from './utils/icon'
+import pinia from './stores/index'
 const app = createApp(App)
 
 // app.use(createPinia())
-app.use(router)
+app.use(router).use(pinia)
 app.use(ElementPlus, {
   locale: zhCn
 })
+
+loadIcon(app)
+
 app.mount('#app')
